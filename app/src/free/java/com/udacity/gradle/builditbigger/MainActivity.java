ab4efb87a7a85d156e0ca86282137d706e0102fa
Katarina Sheremet <kate.romanowskaya@gmail.com>
2018-06-14 13:55:01 +0200

    Show interstitial ad

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -2,11 +2,16 @@ package com.udacity.gradle.builditbigger;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 import com.udacity.gradle.builditbigger.task.EndpointsAsyncTask;
 import com.udacity.gradle.builditbigger.task.JokeReceivedCallback;
 
@@ -14,6 +19,10 @@ import ch.builditbigger.displayjokes.DisplayJokeActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private InterstitialAd mInterstitialAd;
 
     private JokeReceivedCallback callback = new JokeReceivedCallback() {
         @Override
@@ -32,8 +41,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        MobileAds.initialize(this,
+                "ca-app-pub-3940256099942544~3347511713");
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId(getString(R.string.ad_unit_id));
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                // Load the next interstitial.
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+                new EndpointsAsyncTask().execute(callback);
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,6 +81,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        new EndpointsAsyncTask().execute(callback);
+        if (mInterstitialAd != null && mInterstitialAd.isLoaded()) {
+            mInterstitialAd.show();
+        } else {
+            Log.w(TAG, "The interstitial ad wasn't loaded yet.");
+            new EndpointsAsyncTask().execute(callback);
+        }
     }
 }
